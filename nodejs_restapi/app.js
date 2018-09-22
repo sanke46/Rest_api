0d259a8b18ca37cfe52bbe7936c4c1e63555104f
sanke46 <shift.of.razum@gmail.com>
2018-09-22 14:02:42 +0300

    NodeJS REST API: Connecting to MySQL

--- nodejs_restapi/app.js
+++ nodejs_restapi/app.js
@@ -1,8 +1,38 @@
 const express = require("express")
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
 
 app.use(morgan('combined'))
+
+app.get('/user/:id', (req, res) => {
+  console.log("Futching user with id: " + req.params.id)
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'bd_for_app'
+  })
+
+  const userId = req.params.id
+  const queryString = "SELECT * FROM users WHERE id = ?"
+  connection.query(queryString, [userId], (err, rows, fields) => {
+    if (err) {
+      console.log("Failed to query for users: " + err)
+      res.sendStatus(500)
+      // throw err
+      return
+    }
+    console.log("I think we fetched users successfully")
+
+    const users = rows.map((row) => {
+      return {firstName: row.first_name, lastName: row.last_name}
+    })
+
+    res.json(users)
+  })
+
+  // res.end()
+})
 
 app.get("/",(req, res) => {
   console.log("Responding to root route")
